acfc163cf7c7cac3e90132e52ec80a96ef7a5d32
Abdul Moeez <91822215+mianabdulmoez@users.noreply.github.com>
2023-08-13 12:57:59 +0500

    Add files via upload

---
+++ Grades.py
@@ -0,0 +1,21 @@
+grade = int(input("Please write down your Marks here ?"))
+if grade > 90:
+    print("Your child got A++")
+elif grade > 80:
+    print("Your child got A+")
+elif grade > 70:
+    print("Your child got A")
+elif grade > 65:
+    print("Your child got B+")
+elif grade > 60:
+    print("Your child got B")
+elif grade > 50:
+    print("Your child got C+")
+elif grade > 45:
+    print("Your child got C")
+elif grade > 40:
+    print("Your child got D")
+elif grade > 35:
+    print("Your child got D*")
+else:
+    print("Your child got F")
